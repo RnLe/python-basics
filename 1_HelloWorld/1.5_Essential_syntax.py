afa5c6dbf7638db5f905125f5a0fa8c7b7a90f40
Rene-Marcel Lehner <rene.lehner@tu-dortmund.de>
2024-04-01 15:50:09 +0200

    added essential syntax

---
+++ 1_HelloWorld/1.5_Essential_syntax.py
@@ -0,0 +1,19 @@
+# Statements
+
+# Statements are usually single-line commands
+print("abc")
+
+# Python is capable of multi-line statements
+print("abc",
+      "def")
+
+# It's also possible to execute multiple statements in one line
+a=1; b=2; c=3
+
+print(a, b, c)
+
+# Alternative
+a, b, c = 4, 5, 6
+# This is more common
+
+print(a, b, c)
