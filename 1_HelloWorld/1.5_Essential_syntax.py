9ed418af5f40f4d375cb0e86c20f69f71a3308a0
Rene-Marcel Lehner <rene.lehner@tu-dortmund.de>
2024-04-01 15:56:17 +0200

    Add .gitignore and update 1.5_Essential_syntax.py

--- 1_HelloWorld/1.5_Essential_syntax.py
+++ 1_HelloWorld/1.5_Essential_syntax.py
@@ -1,13 +1,13 @@
-# Statements
+# Essential syntax
 
 # Statements are usually single-line commands
 print("abc")
 
-# Python is capable of multi-line statements
+# But python is capable of multi-line statements
 print("abc",
       "def")
 
-# It's also possible to execute multiple statements in one line
+# It's also possible to execute multiple statements in one line (note ; as separator)
 a=1; b=2; c=3
 
 print(a, b, c)
@@ -17,3 +17,8 @@ a, b, c = 4, 5, 6
 # This is more common
 
 print(a, b, c)
+
+# Files can easily be created and written to using print (not even a module needed)
+f = open("1.5_out.txt", "w")
+print("Written to a file. Just like that.", file=f)
+f.close()
