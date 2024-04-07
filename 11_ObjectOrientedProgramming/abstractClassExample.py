baf793b1472239efeaf75345132313e5c440907f
Rene-Marcel Lehner <rene.lehner@tu-dortmund.de>
2024-04-07 12:45:30 +0200

    Add abstract class example with Rectangle and Circle subclasses

--- 11_ObjectOrientedProgramming/abstractClassExample.py
+++ 11_ObjectOrientedProgramming/abstractClassExample.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import math
 
 class Geometry2D(ABC):
     def __init__(self):
@@ -30,4 +31,20 @@ class Rectangle(Geometry2D):
         return 2 * (self.x + self.y)
     
     def get_center(self):
-        return 
+        # Assume the center at the top left corner
+        return (self.x/2, self.y/2)
+    
+class Circle(Geometry2D):
+    def __init__(self, r):
+        super().__init__()
+        self.r = r
+        
+    def get_area(self):
+        return math.pi * self.r**2
+    
+    def get_perimeter(self):
+        return 2*math.pi*self.r
+    
+    def get_center(self):
+        # Assume the center already in the middle (draw the circle around it)
+        return (0,0)
